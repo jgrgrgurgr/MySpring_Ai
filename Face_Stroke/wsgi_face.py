d130d669c4df8d119a8c5db4aee7726202b8e49a
juyounglee08 <coderjuyoung82@gmail.com>
2025-04-19 13:39:07 +0900

    fix cloudtype을 통한 배포를 위해 수정3

--- Face_Stroke/wsgi_face.py
+++
@@ -1,4 +0,0 @@
-from Face_Stroke.Face_Whether_Stroke import app
-
-if __name__ == "__main__":
-    app.run()
--- Pose_Stroke/wsgi_arm.py
+++
@@ -1,4 +0,0 @@
-from Pose_Stroke.Arm_Whether_Stroke import app
-
-if __name__ == "__main__":
-    app.run()
