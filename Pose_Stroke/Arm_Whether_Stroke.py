d976a72539cdd518d71aa0d214342e75d765e450
juyounglee08 <coderjuyoung82@gmail.com>
2025-04-20 10:14:04 +0900

    refactor 전체 코드 최적화3

--- Pose_Stroke/Arm_Whether_Stroke.py
+++ Pose_Stroke/Arm_Whether_Stroke.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 # MoveNet 모델 로드 (전역 변수로 사용)
 movenet = hub.load("https://tfhub.dev/google/movenet/singlepose/thunder/4")
 movenet = movenet.signatures['serving_default']
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 tf.config.set_visible_devices([], 'GPU')  # GPU 가속 비활성화
 
 class StrokePredictor:
