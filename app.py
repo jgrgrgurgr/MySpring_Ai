869d7fdbfa5a13282fb598153e472e2296f8eeed
juyounglee08 <coderjuyoung82@gmail.com>
2025-04-20 15:11:36 +0900

    refactor 핵심 코드(Arm_Whether_Stroke.py, Face_Whether_Stroke.py) Model_Hub 디렉토리 (재)통합2

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ tf.config.set_visible_devices([], 'GPU')
 
 # 상대 경로 대신 절대 경로 사용 (기존 코드 유지)
 from Model_Hub.Face_Stroke.Face_Whether_Stroke import StrokePredictor as ImageStrokePredictor
-from Model_Hub.Pose_Stroke.Arm_Whether_Stroke import StrokePredictor as PoseStrokePredictor
+from Model_Hub.Pose_Stroke2.Arm_Whether_Stroke import StrokePredictor as PoseStrokePredictor
 
 app = Flask(__name__)
 
